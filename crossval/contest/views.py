cb4499f158742f314b3c3ebbb09412ad98538a52
Arjoonn Sharma <arjoonn.94@gmail.com>
2016-08-20 15:24:40 +0530

    html files for contest

--- crossval/contest/views.py
+++ crossval/contest/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from contest import models
 
-# Create your views here.
+def contests_home(request):
+    """
+    Display active contests and general information.
+    """
+    pass
+
+def contest(request, pk):
+    """
+    Contest specific details.
+    """
+    pass
+
+def contest_lb(request, pk):
+    """
+    Leaderboard
+    """
+    pass
+
+def contest_submit(request, pk):
+    """
+    Submission page for contest
+    """
+    pass
